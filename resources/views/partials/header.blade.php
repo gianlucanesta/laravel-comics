32234c3e59e6655bdf1d1cdeff4d8cbeab824043
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2022-02-10 21:08:46 +0100

    jumbotron

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,44 +1,67 @@
 <header>
-        
-    <div class="container">
-        <div class="logo">
-            <img src="{{ asset('images/dc-logo.png')}}" alt="Logo DC">
+    <div class="header-top">
+        <div class="container">
+            <nav>
+                <ul>
+                    <li>
+                        <a href=""></a>
+                    </li>
+                    <li>
+                        <a href=""></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+    <div class="header-menu">
+        <div class="container">
+            <div class="logo">
+                <img src="{{ asset('images/dc-logo.png')}}" alt="Logo DC">
+            </div>
+    
+            <nav>
+                <ul class="menu-list">
+                    <li>
+                        <a href="#">characters</a>
+                    </li> 
+                    <li>
+                        <a  class="current" href="#">comics</a>
+                    </li>    
+                    <li>
+                        <a href="#">movies</a>
+                    </li>    
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+                    <li>
+                        <a href="#">games</a>
+                    </li>    
+                    <li>
+                        <a href="#">collectibles</a>
+                    </li>    
+                    <li>
+                        <a href="#">videos</a>
+                    </li>    
+                    <li>
+                        <a href="#">fans</a>
+                    </li>    
+                    <li>
+                        <a href="#">news</a>
+                    </li>    
+                    <li>
+                        <a href="#">shop</a>
+                    </li>                         
+                </ul>
+            </nav>
         </div>
 
-        <nav>
-            <ul class="menu-list">
-                <li>
-                    <a href="#">characters</a>
-                </li> 
-                <li>
-                    <a  class="current" href="#">comics</a>
-                </li>    
-                <li>
-                    <a href="#">movies</a>
-                </li>    
-                <li>
-                    <a href="#">tv</a>
-                </li>
-                <li>
-                    <a href="#">games</a>
-                </li>    
-                <li>
-                    <a href="#">collectibles</a>
-                </li>    
-                <li>
-                    <a href="#">videos</a>
-                </li>    
-                <li>
-                    <a href="#">fans</a>
-                </li>    
-                <li>
-                    <a href="#">news</a>
-                </li>    
-                <li>
-                    <a href="#">shop</a>
-                </li>                         
-            </ul>
-        </nav>
+    </div>
+
+    <div class="header-jumbo">
+        <div class="content">
+            <img src="{{ asset('images/jumbotron.jpg')}}" alt="">
+        </div>     
     </div>
     
 </header>
